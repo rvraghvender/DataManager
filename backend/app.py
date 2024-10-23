63a1c0b49c73456bb70ce209a9bd9bb3b18641e1
Raghvender <rvraghvender@gmail.com>
2024-10-23 18:43:57 +0200

    Stored database/logs in root folder

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
-from config.config import UPLOAD_FOLDER
+from config.config import UPLOAD_FOLDER, MONGO_URI
 from routes.file_routes import file_bp
 import os
+import time
+import subprocess
+import logging
+from routes.file_routes import file_bp
+from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.INFO)
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Path to the MongoDB configuration file
+MONGOD_CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'backend/config/mongod.conf')
+
+# Check if MongoDB is running and start it if not
+def check_and_start_mongodb():
+    try:
+        output = subprocess.check_output(["pgrep", "-f", "mongod"], stderr=subprocess.STDOUT)
+        if output:
+            logging.info("MongoDB is running.")
+            return None
+    except subprocess.CalledProcessError:
+        logging.error("MongoDB is not running. Attempting to start it...")
+
+    # Start MongoDB with the specified config file
+    try:
+        mongo_process = subprocess.Popen(["mongod", "--config", MONGOD_CONFIG_PATH])
+        logging.info("MongoDB started with config file: %s", MONGOD_CONFIG_PATH)
+        return mongo_process
+    except Exception as e:
+        logging.error("Failed to start MongoDB: %s", str(e))
+        return None
+
+
+def stop_mongodb(process):
+    if process:
+        process.terminate()
+        process.wait()
 
 app = Flask(__name__, static_folder='../frontend', template_folder='../frontend')
 
@@ -13,7 +51,6 @@ CORS(app)
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-from flask import render_template
 
 @app.route('/')
 def index():
@@ -23,6 +60,17 @@ def index():
 app.register_blueprint(file_bp, url_prefix='/api/files')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+
+    # Run the check when the app starts
+    mongo_process = check_and_start_mongodb()
+
+    time.sleep(5)
+    
+    try:
+        app.run(debug=True, port=5000)
+    except KeyboardInterrupt:
+        logging.info("Shutting down MongoDB...")
+        stop_mongodb(mongo_process)
+        logging.info("MongoDB shut down successfully.")
 
 
